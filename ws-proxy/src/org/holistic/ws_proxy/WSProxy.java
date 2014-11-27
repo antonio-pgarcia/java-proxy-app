25761b67eaa07c8d4894b78142ee1e2a006703a9
Antonio Prestes García <antonio.garcia2@gmail.com>
2014-11-27 13:08:27 +0100

    Update WSProxy.java

--- ws-proxy/src/org/holistic/ws_proxy/WSProxy.java
+++ ws-proxy/src/org/holistic/ws_proxy/WSProxy.java
@@ -53,96 +53,70 @@ import javax.servlet.http.*;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-// Referenced classes of package org.holistic.ws_proxy:
-//            WSProxyHelper
 
-public class WSProxy extends HttpServlet implements Servlet
-{
-
-    public WSProxy()
-    {
+public class WSProxy extends HttpServlet implements Servlet {
+    private WSProxyHelper m_objWSProxyHelper;
+    private Log log;
+    
+    public WSProxy() {
         log = LogFactory.getLog("[WSProxy]");
     }
 
-    public void init()
-        throws ServletException
-    {
+    public void init() throws ServletException {
         String m_strEndPoint = null;
         String m_strTimeOut = null;
         String m_strProxyHost = null;
         String m_strProxyPort = null;
+        
         m_strEndPoint = getInitParameter("endpoint");
-        log.debug("Recuperamos parametro de inicializacion del Proxy: endPoint = " + m_strEndPoint);
         m_strTimeOut = getInitParameter("timeout");
-        log.debug("Recuperamos parametro de inicializacion del Proxy: timeout = " + m_strTimeOut);
-        m_strProxyHost = getInitParameter("proxyHost");
-        log.debug("Recuperamos parametro de inicializacion del Proxy: proxyHost = " + m_strProxyHost);
+        m_strProxyHost = getInitParameter("proxyHost");        
         m_strProxyPort = getInitParameter("proxyPort");
-        log.debug("Recuperamos parametro de inicializacion del Proxy: proxyPort = " + m_strProxyPort);
-        if(m_strEndPoint != null && m_strTimeOut != null)
-        {
+        
+        log.debug("Init parameter::endPoint---> " + m_strEndPoint);
+        log.debug("Init parameter::timeout----> " + m_strTimeOut);
+        log.debug("Init parameter::proxyHost--> " + m_strProxyHost);
+        log.debug("Init parameter::proxyPort--> " + m_strProxyPort);
+
+        if(m_strEndPoint != null && m_strTimeOut != null) {
             m_objWSProxyHelper = new WSProxyHelper(m_strEndPoint, Integer.parseInt(m_strTimeOut));
             if(m_strProxyHost != null && m_strProxyPort != null && !m_strProxyHost.equals("") && !m_strProxyPort.equals(""))
                 m_objWSProxyHelper.set_proxy(m_strProxyHost, m_strProxyPort);
         } else
-        {
-            log.error("Parametos invalidos (endpoint or timeout)");
-        }
+            log.error("Invalid parameter (endpoint|timeout)");
     }
 
-    protected WSProxyHelper get_proxyhelper()
-        throws Exception
-    {
-        if(m_objWSProxyHelper == null)
-        {
-            log.info("Proxy no inicializado");
-            throw new Exception("Proxy no inicializado");
+    protected WSProxyHelper get_proxyhelper() throws Exception {
+        if(m_objWSProxyHelper == null) {
+            log.info("Java-Proxy-App Not initialized"); 
+            throw new Exception("Java-Proxy-App Not initialized");
         } else
-        {
             return m_objWSProxyHelper;
-        }
     }
 
-    protected void set_proxyhelper(WSProxyHelper objValue)
-    {
+    protected void set_proxyhelper(WSProxyHelper objValue) {
         m_objWSProxyHelper = objValue;
     }
 
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException
-    {
-        log.info("Accedo");
-        try
-        {
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+        try {
             get_proxyhelper().processRequest(request, response);
-        }
-        catch(Exception m_objException)
-        {
-            log.info("Se ha producido una excepcion: " + m_objException.getMessage());
-            try
-            {
+        } catch(Exception m_objException) {
+            log.info("An exception has been catched: " + m_objException.getMessage());
+            try {
                 response.sendError(502, "Error");
-            }
-            catch(IOException m_objIOException)
-            {
-                log.info("Se ha producido una excepcion al establecer el error 504: " + m_objIOException.getMessage());
+            } catch(IOException m_objIOException) {
+                log.info("Exception catched setting http status code: " + m_objIOException.getMessage());
                 throw new ServletException("[WSProxy] " + m_objIOException.getMessage());
             }
         }
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, IOException
-    {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, IOException
-    {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         processRequest(request, response);
     }
-
-    private WSProxyHelper m_objWSProxyHelper;
-    private Log log;
 }
